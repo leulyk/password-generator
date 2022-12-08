50566a926a90096db26f4e9599f782856508b910
Leul Yiheyis <leulykpro@gmail.com>
2022-12-08 14:37:03 +0300

    add pouchDb

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@
 import { render } from 'solid-js/web';
 import './index.css';
 import App from './App';
+import * as PouchDB from "pouchdb";
 
 render(() => <App />, document.getElementById('app'));
